a2d8c1e5b0bc81aa3c1b9909a940eec778321afe
algjohnston <agjohnston112@gmail.com>
2022-12-23 18:53:38 -0600

    PinkyPlayer 0.30
    Gets appropriate permission to proceed to query the MediaStore for music files.

--- app/src/androidTest/java/com/fourthfinger/pinkyplayer/FragmentSongsTest.kt
+++
@@ -1,8 +0,0 @@
-package com.fourthfinger.pinkyplayer
-
-class FragmentSongsTest {
-
-    // Make sure all songs in playlist have files when testing FragmentSongs
-
-
-}
--- app/src/main/java/com/fourthfinger/pinkyplayer/ApplicationMain.kt
+++
@@ -1,7 +0,0 @@
-package com.fourthfinger.pinkyplayer
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class ApplicationMain: Application()
